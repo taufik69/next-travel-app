cf8085b50f82de48670edd630be7b9e0fdf9af4f
taufik69 <taufik35-2915@diu.edu.bd>
2023-10-26 19:53:25 +0600

    camp part

--- app/Component/Camp.jsx
+++ app/Component/Camp.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import BannerLayout from "./CommonComponent/BannerLayout";
 import React from "react";
 
 const Camp = () => {
@@ -6,65 +7,16 @@ const Camp = () => {
     <>
       <section className="pt-10">
         <div className="container mx-auto">
-          <div className="lg:w-10/12 relative">
-            <Image
-              src={"/campbanner.png"}
-              alt="campbanner.png"
-              width={1100}
-              height={300}
-              className="rounded-2xl"
-            />
-            <div className="flex absolute top-5 left-5 gap-3  ">
+          <div className=" mt-10 flex flex-col gap-4 lg:flex lg:gap-6">
+            <BannerLayout image={"/campbanner.png"} />
+
+            <div className="lg:w-1/2">
               <Image
-                src={"/map.svg"}
-                alt="camp.png"
-                width={10}
-                height={10}
-                className="p-2 rounded-full bg-[#30AF5B] w-[55px] h-[55px]"
+                src={"/campbanner2.png"}
+                width={2100}
+                height={2000}
+                className="h-full object-cover rounded-2xl"
               />
-              <div className="font-inter text-white">
-                <h1 className="font-semibold text-base ">Putuk Truno Camp</h1>
-                <p className="font-normal text-sm">Prigen, Pasuruan</p>
-              </div>
-            </div>
-
-            <div className="flex items-center gap-x-3 absolute bottom-6 left-5">
-              <div className="flex -space-x-4 items-center">
-                <Image
-                  src={"/person-1.png"}
-                  alt="camp.png"
-                  width={150}
-                  height={150}
-                  className=" rounded-full w-[50px] h-[50px]"
-                />
-
-                <Image
-                  src={"/person-1.png"}
-                  alt="camp.png"
-                  width={100}
-                  height={100}
-                  className=" rounded-full w-[55px] h-[55px]"
-                />
-
-                <Image
-                  src={"/person-1.png"}
-                  alt="camp.png"
-                  width={100}
-                  height={100}
-                  className=" rounded-full w-[55px] h-[55px]"
-                />
-
-                <Image
-                  src={"/person-1.png"}
-                  alt="camp.png"
-                  width={100}
-                  height={100}
-                  className=" rounded-full w-[55px] h-[55px]"
-                />
-              </div>
-              <div className="font-inter text-xm font-bold">
-                <h1>50+ Joined</h1>
-              </div>
             </div>
           </div>
         </div>
---
+++ app/Component/CommonComponent/BannerLayout.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+const BannerLayout = ({ image }) => {
+  return (
+    <>
+      <div className="lg:w-10/12 relative">
+        <Image
+          src={image}
+          alt="campbanner.png"
+          width={1100}
+          height={300}
+          className="rounded-2xl"
+        />
+        <div className="flex absolute top-5 left-5 gap-3  ">
+          <Image
+            src={"/map.svg"}
+            alt="camp.png"
+            width={10}
+            height={10}
+            className="p-2 rounded-full bg-[#30AF5B] w-[55px] h-[55px]"
+          />
+          <div className="font-inter text-white">
+            <h1 className="font-semibold text-base ">Putuk Truno Camp</h1>
+            <p className="font-normal text-sm">Prigen, Pasuruan</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-x-3 absolute bottom-6 left-5">
+          <div className="flex -space-x-4 items-center">
+            <Image
+              src={"/person-1.png"}
+              alt="camp.png"
+              width={150}
+              height={150}
+              className=" rounded-full w-[50px] h-[50px]"
+            />
+
+            <Image
+              src={"/person-1.png"}
+              alt="camp.png"
+              width={100}
+              height={100}
+              className=" rounded-full w-[55px] h-[55px]"
+            />
+
+            <Image
+              src={"/person-1.png"}
+              alt="camp.png"
+              width={100}
+              height={100}
+              className=" rounded-full w-[55px] h-[55px]"
+            />
+
+            <Image
+              src={"/person-1.png"}
+              alt="camp.png"
+              width={100}
+              height={100}
+              className=" rounded-full w-[55px] h-[55px]"
+            />
+          </div>
+          <div className="font-inter text-xm font-bold">
+            <h1>50+ Joined</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BannerLayout;
